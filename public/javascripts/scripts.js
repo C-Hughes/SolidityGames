3390b9ea4f082b98e82d4d981b4a47896c667f33
CHughes <->
2018-10-13 18:15:44 +0100

    First Commit

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,91 @@
+//Show/hide element
+function toggleElement(param) {
+    var x = document.getElementById(param);
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+//Function to set localstorage & change style wh en day/night toggle is pressed.
+function toggleDayNight(changeTo){
+    //Date color for BIGX tag
+    var BIGXDateBackground = "#f1373a";
+    var BIGXTextColor = "white";
+
+    if (changeTo == "day"){
+        //Set Colours
+        var primaryDay = "#F5F6FA";
+        var secondaryDay = "#FFFFFF";
+        var headerDay = "#F5F5F5";
+        var textColorDay = "#172130";
+
+        //Change theme to day colours
+        document.getElementById("dayNightBtn").innerHTML = '<div class="w3-card-2 w3-button secondary" title="Night Mode" onclick="toggleDayNight(\'night\')"><i class="far fa-moon" aria-hidden="true"></i></div>';
+
+        var elements = document.getElementsByClassName("primary");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor=primaryDay;
+            elements[i].style.color=textColorDay;
+        }
+        elements = document.getElementsByClassName("secondary");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor=secondaryDay;
+            elements[i].style.color=textColorDay;
+        }
+
+        elements = document.getElementsByClassName("header");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor=headerDay;
+            elements[i].style.color=textColorDay;
+        }
+        localStorage.setItem("theme", "day");
+    } else {
+        //Change theme to night colours
+        //Set Colors
+        var primaryNight = "#1A1B1F";
+        var secondaryNight = "#202126";
+        var headerNight = "#2A2B31";
+        var textColorNight = "#A1A29A";
+
+        document.getElementById("dayNightBtn").innerHTML = '<div class="w3-card-2 w3-button secondary" title="Day Mode" onclick="toggleDayNight(\'day\')"><i class="far fa-sun" aria-hidden="true"></i></div>';
+
+        var elements = document.getElementsByClassName("primary");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor=primaryNight;
+            elements[i].style.color=textColorNight;
+        }
+        elements = document.getElementsByClassName("secondary");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor=secondaryNight;
+            elements[i].style.color=textColorNight;
+        }
+
+        elements = document.getElementsByClassName("header");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.backgroundColor=headerNight;
+            elements[i].style.color=textColorNight;
+        }
+        localStorage.setItem("theme", "night");
+    }
+
+    //Set Date colour for BIGX tag
+    elements = document.getElementsByClassName("BIGX");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].childNodes[1].style.backgroundColor=BIGXDateBackground;
+        elements[i].childNodes[1].style.color=BIGXTextColor;
+        elements[i].style.fontWeight = 'bold';
+    }
+};
+
+
+//Set local storage if not initialised
+if (!localStorage.getItem("theme")){
+    localStorage.setItem("theme", "night");
+} else {
+    //Set day/night
+    toggleDayNight(localStorage.getItem("theme"));
+}
+//Update day/night
+toggleDayNight(localStorage.getItem("theme"));
