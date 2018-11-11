4e6a484ec942d49410aa0573c9f500e35e2327c9
CHughes <->
2018-11-11 21:11:04 +0000

    Updates

--- routes/games.js
+++ routes/games.js
@@ -5,4 +5,8 @@ router.get('/warmUp', function(req, res, next) {
     res.render('games/warmUp', { title: '1. Warm Up'});
 });
 
+router.get('/odometer', function(req, res, next) {
+    res.render('games/odometer', { title: '2. Odometer'});
+});
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Solidity Wargames' });
+  res.render('index', { title: 'Solidity Games' });
 });
 
 module.exports = router;
