dbcf71e675fa0915e7e773159468d3ebd674fa44
CHughes <->
2019-01-19 11:29:32 +0000

    Display Current Network

--- public/javascripts/web3.js
+++ public/javascripts/web3.js
@@ -8,6 +8,7 @@ window.addEventListener('load', async () => {
         // Acccounts now exposed
         console.log('MetaMask Connected');
         getBalance();
+        getNetwork();
         //web3.eth.sendTransaction({/* ... */});
     } catch (error) {
         // User denied account access...
@@ -31,11 +32,47 @@ function getBalance() {
             web3.eth.getBalance(accounts[0], function(error, wei) {
                 if (!error) {
                     web3.eth.defaultAccount = accounts[0];
-                    var balance = web3.fromWei(wei, 'ether').toFixed(2);
+                    var balance = web3.fromWei(wei, 'ether').toFixed(3);
                     document.getElementById("etherBalance").innerHTML = "Ξ " + balance;
                 }
             })
         });
+    } catch (err) {
+        document.getElementById("etherBalance").innerHTML = err;
+    }
+}
+
+function getNetwork() {
+    var network = "Not Connected";
+    try {
+        web3.version.getNetwork((err, netId) => {
+            switch (netId) {
+            case "1":
+                network = "Mainnet";
+                console.log('This is mainnet')
+                break
+            case "2":
+                network = "Morden Deprecated Network";
+                console.log('This is the deprecated Morden test network.')
+                break
+            case "3":
+                network = "Ropsten Network";
+                console.log('This is the ropsten test network.')
+                break
+            case "4":
+                network = "Rinkeby Network";
+                console.log('This is the Rinkeby test network.')
+                break
+            case "42":
+                network = "Kovan Network";
+                console.log('This is the Kovan test network.')
+                break
+            default:
+                network = "Unknown Network";
+                console.log('This is an unknown network.')
+            }
+            document.getElementById("etherNetwork").innerHTML = network;
+        })
     } catch (err) {
         document.getElementById("etherBalance").innerHTML = err;
     }
