02f33afa746ce4dd1431ac3479aadb82a869b2c8
CHughes <->
2019-02-23 17:47:08 +0000

    Update survey database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var survey = require('../models/survey');
 
 router.get('/welcome', function(req, res, next) {
     res.render('pages/welcome', { title: 'Welcome Page' });
@@ -47,6 +48,39 @@ router.get('/securitytools', function(req, res, next) {
 
 router.get('/survey', function(req, res, next) {
     res.render('pages/survey', { title: 'Solidity Games' });
+});
+
+router.post('/survey', function (req, res, next) {
+    var Survey = new survey({
+        Gender: req.body.Gender,
+        Nationality: req.body.Nationality,
+        University: req.body.University,
+        Occupation: req.body.Occupation,
+        ProgramingExp: req.body.ProgramingExp,
+        ProgramingLang: req.body.ProgramingLang,
+        Ethereum: req.body.Ethereum,
+        Solidity: req.body.Solidity,
+        OverallImpression: req.body.OverallImpression,
+        Navigate: req.body.Navigate,
+        Instructions: req.body.Instructions,
+        Understand: req.body.Understand,
+        Interact: req.body.Interact,
+        Help: req.body.Help,
+        TeachingAbility: req.body.TeachingAbility,
+        ImprovedInterest: req.body.ImprovedInterest,
+        ChallengesImproved: req.body.ChallengesImproved,
+        OverallImproved: req.body.OverallImproved,
+        OtherFeedback: req.body.OtherFeedback,
+        LocalStorage: req.body.LocalStorage
+    });
+    Survey.save(function (err, result) {
+        if(err){
+            res.redirect('/survey');
+        } else {
+            res.redirect('/');
+        }
+
+    });
 });
 
 module.exports = router;
