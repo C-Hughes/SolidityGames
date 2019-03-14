8728d389a082d85283e483fadb2b6dc24fdd8bab
CHughes <->
2019-03-14 17:08:51 +0000

    Can give feedback for each individual level

--- routes/games.js
+++ routes/games.js
@@ -31,12 +31,12 @@ router.get('/forcedPayments', function(req, res, next) {
     res.render('games/forcedPayments', { title: '6. Forced Payments'});
 });
 
-router.get('/simpleDAO', function(req, res, next) {
-    res.render('games/simpleDAO', { title: '7. simpleDAO'});
+router.get('/randomness', function(req, res, next) {
+    res.render('games/randomness', { title: '7. Randomness'});
 });
 
-router.get('/randomness', function(req, res, next) {
-    res.render('games/randomness', { title: '8. Randomness'});
+router.get('/simpleDAO', function(req, res, next) {
+    res.render('games/simpleDAO', { title: '8. simpleDAO'});
 });
 
 
@@ -48,8 +48,107 @@ router.post('/warmUp', function(req, res, next) {
     levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
         if(err){
             console.log("Something wrong when updating data!");
+            res.redirect('/games/warmUp');
         } else {
             res.redirect('/games/warmUp');
+        }
+    });
+});
+
+router.post('/rubixi', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {Rubixi: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/rubixi');
+        } else {
+            res.redirect('/games/rubixi');
+        }
+    });
+});
+
+router.post('/odometer', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {Odometer: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/odometer');
+        } else {
+            res.redirect('/games/odometer');
+        }
+    });
+});
+
+router.post('/keepingSecrets', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {KeepingSecrets: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/keepingSecrets');
+        } else {
+            res.redirect('/games/keepingSecrets');
+        }
+    });
+});
+
+router.post('/externalFactors', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {ExternalFactors: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/externalFactors');
+        } else {
+            res.redirect('/games/externalFactors');
+        }
+    });
+});
+
+router.post('/forcedPayments', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {ForcedPayments: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/forcedPayments');
+        } else {
+            res.redirect('/games/forcedPayments');
+        }
+    });
+});
+
+router.post('/randomness', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {Randomness: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/randomness');
+        } else {
+            res.redirect('/games/randomness');
+        }
+    });
+});
+
+router.post('/simpleDAO', function(req, res, next) {
+    var query = { Session: req.sessionID};
+    var update = {SimpleDAO: req.body.levelFeedback};
+    var options = {upsert: true, new: true, setDefaultsOnInsert: true};
+    levelFeedback.findOneAndUpdate(query, update, options, function(err, doc){
+        if(err){
+            console.log("Something wrong when updating data!");
+            res.redirect('/games/simpleDAO');
+        } else {
+            res.redirect('/games/simpleDAO');
         }
     });
 });
